47689fd9c73f202e701b66ece3061a570f8eac9a
ali-haider-dev <75415715+ali-haider-dev@users.noreply.github.com>
2021-08-11 15:33:05 +0500

    biuld tha navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout Page</h1>
+          </Route>
+          <Route path="/login">
+            <h1> LogIn Page</h1>
+            <Link to="/">
+              <button>Go back to home page</button>
+            </Link>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Home Page!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase               "
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBULuY1zVH6IZAeTUITX70fDoIM8VuKWOE",
+    authDomain: "e-clone-782d0.firebaseapp.com",
+    projectId: "e-clone-782d0",
+    storageBucket: "e-clone-782d0.appspot.com",
+    messagingSenderId: "844178664071",
+    appId: "1:844178664071:web:c5bf505fddc5506291e622",
+    measurementId: "G-MQCX4DB3QG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
